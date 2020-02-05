882e784b9048fd37f55f1d63f6a8642e32b7042a
huangjin <15197155663@163.com>
2020-02-05 17:22:31 +0800

    增加动画

--- src/pages/page1/index.js
+++ src/pages/page1/index.js
@@ -1,5 +1,15 @@
-const mySwiper = new Swiper ('.swiper-container', {
-   direction: 'vertical', // 垂直切换选项
+const mySwiper = new Swiper('.swiper-container', {
+  direction: 'vertical', // 垂直切换选项
+  on: {
+    init: function() {
+      swiperAnimateCache(this) //隐藏动画元素
+      swiperAnimate(this) //初始化完成开始动画
+    },
+    slideChangeTransitionEnd: function() {
+      swiperAnimate(this) //每个slide切换结束时也运行当前slide动画
+      // this.slides.eq(this.activeIndex).find('.ani').removeClass('ani'); // 动画只展现一次，去除ani类名
+    }
+  }
   /* loop: true, // 循环模式选项
   
   // 如果需要分页器
@@ -17,4 +27,4 @@ const mySwiper = new Swiper ('.swiper-container', {
   scrollbar: {
     el: '.swiper-scrollbar',
   }, */
-})        
+})
